c25aeaf5278593b0fe6939098c023738872ebef7
Joe Hildebrand <joe-github@cursive.net>
2021-02-22 14:30:47 -0700

    Add tests

---
+++ bin/data-colon.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+'use strict'
+
+const fs = require('fs')
+const dataUri = require('strong-data-uri')
+const mime = require('mime')
+const commander = require('commander')
+
+// decode: data-colon data:foo
+// encode: data-colon filename
+// decode: data-colon < filename-starting-with-data:
+// encode: data-colon < filename
+
+const pkg = require('../package')
+const {program} = commander
+
+program
+  .version(pkg.version)
+  .option('-o, --output <file>', 'Output file name [stdout]')
+  .option('-m, --mediatype <type>', 'MIME media type for encoding')
+  .arguments('[fileOrURI...]')
+  .parse(process.argv)
+
+const opts = program.opts()
+
+const DATA = 'data:'
+const DATA_REGEX = /^data:/
+const DATA_BUF = Buffer.from(DATA)
+
+const OUTPUT = opts.output ?
+  fs.createWriteStream(opts.output) :
+  process.stdout
+
+let media = opts.mediatype
+if (program.args.length === 0) {
+  program.args = ['-']
+}
+
+function readStdin(cb) {
+  return new Promise((resolve, reject) => {
+    const input = []
+    process.stdin.on('data', buf => input.push(buf))
+    process.stdin.on('end', () => resolve(Buffer.concat(input)))
+    process.stdin.on('error', reject)
+
+    process.stdin.resume()
+  })
+}
+
+function encode(buf) {
+  // encode only throws on bad input types, which can't happen here.
+  OUTPUT.write(dataUri.encode(buf, media))
+  OUTPUT.write('\n')
+}
+
+function decode(buf) {
+  let dat = Buffer.isBuffer(buf) ? buf.toString('utf8') : buf
+  dat = dat.trim()
+  return new Promise((resolve, reject) => {
+    try {
+      OUTPUT.write(dataUri.decode(dat))
+    } catch (e) {
+      console.error(e.message)
+      process.exit(1)
+    }
+  })
+}
+
+function detect(buf) {
+  const f = (
+    (buf.length < 5) ||
+    (!DATA_BUF.equals(buf.slice(0, DATA_BUF.length)))) ?
+    encode :
+    decode
+  f(buf)
+}
+
+async function main() {
+  for (const arg of program.args) {
+    if (arg.match(DATA_REGEX)) {
+      await decode(arg)
+    } else {
+      let buf = null
+      if (arg === '-') {
+        buf = await readStdin()
+      } else {
+        if (!media) {
+          media = mime.getType(arg)
+        }
+        buf = await fs.promises.readFile(arg)
+      }
+      await detect(buf)
+    }
+  }
+}
+
+main().catch(er => {
+  console.error(er.message)
+  process.exit(1)
+})
---
+++ test/exec.ava.js
@@ -0,0 +1,166 @@
+'use strict'
+
+const test = require('ava')
+const {spawn} = require('child_process')
+const path = require('path')
+const util = require('util')
+const fs = require('fs')
+const os = require('os')
+
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+const mkdtemp = util.promisify(fs.mkdtemp)
+const rmdir = util.promisify(fs.rmdir)
+const pkg = require('../package.json')
+
+async function withTempDir(f) {
+  const dir = await mkdtemp(path.join(os.tmpdir(), pkg.name) + '-')
+  try {
+    await f(dir)
+  } finally {
+    await rmdir(dir, {recursive: true})
+  }
+}
+
+function exec(bin, opts = {}) {
+  opts = {
+    args: [],
+    encoding: 'utf8',
+    env: {},
+    ...opts
+  }
+  return new Promise((resolve, reject) => {
+    bin = path.join(__dirname, '..', 'bin', bin + '.js')
+    const env = {
+      ...process.env,
+      ...opts.env
+    }
+    const c = spawn(bin, opts.args, {
+      stdio: 'pipe',
+      env
+    })
+    c.on('error', reject)
+    const bufs = []
+    c.stdout.on('data', b => bufs.push(b))
+    c.stderr.on('data', b => bufs.push(b))
+    c.on('close', code => {
+      const buf = Buffer.concat(bufs)
+      const str = buf.toString(opts.encoding)
+      if (code !== 0) {
+        const err = new Error(`process fail, code ${code}`)
+        err.buf = buf
+        err.str = str
+        reject(err)
+      } else {
+        resolve(str)
+      }
+    })
+    c.on('exit', (code, signal) => {
+      const problem = code || signal
+      if (problem) {
+        reject(new Error(`Invalid exit: ${problem}`))
+      }
+    })
+    if (opts.stdin != null) {
+      c.stdin.write(opts.stdin)
+    }
+    c.stdin.end()
+  })
+}
+
+test('help', async t => {
+  const txt = await exec('data-colon', {
+    args: ['-h']
+  })
+  t.is(txt, `\
+Usage: data-colon [options] [fileOrURI...]
+
+Options:
+  -V, --version           output the version number
+  -o, --output <file>     Output file name [stdout]
+  -m, --mediatype <type>  MIME media type for encoding
+  -h, --help              display help for command\n`)
+})
+
+test('version', async t => {
+  const txt = await exec('data-colon', {
+    args: ['-V']
+  })
+  t.is(txt, pkg.version + '\n')
+})
+
+test('decode', async t => {
+  let txt = await exec('data-colon', {
+    args: ['data:text/plain;base64,aGVsbG8gd29ybGQ=']
+  })
+  t.is(txt, 'hello world')
+  txt = await exec('data-colon', {
+    stdin: 'data:text/plain;base64,aGVsbG8gd29ybGQ='
+  })
+  t.is(txt, 'hello world')
+
+  await t.throwsAsync(async() => {
+    await exec('data-colon', {
+      args: ['data:text/plain,hello\nworld']
+    })
+  })
+})
+
+test('encode', async t => {
+  const txt = await exec('data-colon', {
+    args: ['-m', 'text/plain'],
+    stdin: 'hello world'
+  })
+  t.is(txt, 'data:text/plain;base64,aGVsbG8gd29ybGQ=\n')
+})
+
+test('round trip', async t => {
+  await withTempDir(async dir => {
+    const foo = path.join(dir, 'foo.json')
+    const bar = path.join(dir, 'bar.json')
+    await writeFile(foo, JSON.stringify({
+      foo: 1,
+      bar: [true, null]
+    }, null, 2))
+    let txt = await exec('data-colon', {
+      args: [foo]
+    })
+    t.is(
+      txt,
+      'data:application/json;base64,' +
+      'ewogICJmb28iOiAxLAogICJiYXIiOiBbCiAgICB0cnVlLAogICAgbnVsbAogIF0KfQ==\n'
+    )
+    txt = await exec('data-colon', {
+      args: [foo, '-m', 'text/plain']
+    })
+    t.is(
+      txt,
+      'data:text/plain;base64,' +
+      'ewogICJmb28iOiAxLAogICJiYXIiOiBbCiAgICB0cnVlLAogICAgbnVsbAogIF0KfQ==\n'
+    )
+    await t.throwsAsync(async() => {
+      await exec('data-colon', {
+        args: [foo + '-DOES_NOT_EXIST']
+      })
+    })
+    await exec('data-colon', {
+      args: [foo, '-o', bar]
+    })
+    t.is(
+      await readFile(bar, 'utf8'),
+      'data:application/json;base64,' +
+      'ewogICJmb28iOiAxLAogICJiYXIiOiBbCiAgICB0cnVlLAogICAgbnVsbAogIF0KfQ==\n'
+    )
+    await exec('data-colon', {
+      args: [bar, '-o', foo]
+    })
+    t.deepEqual(JSON.parse(await readFile(foo, 'utf8')), {
+      foo: 1,
+      bar: [true, null]
+    })
+    await writeFile(foo, '{}')
+    t.is(await exec('data-colon', {
+      args: [foo]
+    }), 'data:application/json;base64,e30=\n')
+  })
+})
